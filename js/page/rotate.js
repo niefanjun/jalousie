2f2583fd79e1d4177f12af9f15eef72cd0ebe52c
聂凡竣 <niefanjun@gmail.com>
2014-05-09 00:30:05 +0800

    v1.0

---
+++ js/page/rotate.js
@@ -0,0 +1,9 @@
+define(function(require, exports, module){
+    var $ = require("jquery");
+    //$('#box').animate({  textIndent: 0 }, {
+    //    step: function(now,fx) {
+    //      $(this).css('-webkit-transform','rotateX('+now+'deg)'); 
+    //    },
+    //    duration: 500
+    //},'linear');
+});
